0645f9c56374ac90982313fffa53311f7ca66a26
Kirby Rodriguez <kirby122402@hotmail.com>
2024-01-18 14:38:20 -0800

    working buttons

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
+import {Routes, Route, useNavigate} from 'react-router-dom';
 import Logo from './components/Logo.js'
 import './App.css';
+import About from './About.js';
+import Testimonials from './Testimonials.js';
+import Contact from './Contact.js';
+export default function App() {
+  
+  return (
+    <div>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/testimonials" element={<Testimonials />} />
+          <Route path="/contactus" element={<Contact />} />
 
-function App() {
+         <Route path="/" element={<Home />} />
+       </Routes>
+</div>
+  );
+}
+
+function Home() {
+  const navigate = useNavigate();
+
+  const navigateAbout = () => {
+    // navigate to /about
+    navigate('/about');
+  };
+
+  const navigateTestimonials = () => {
+    // navigate to /testimonials
+    navigate('/testimonials');
+  };
+
+  const navigateContactUs = () => {
+    // navigate to /testimonials
+    navigate('/contactus');
+  };
   return (
     <div className="App">
-      <Logo>The Big Yellow Sulphur Pile</Logo>
+      <Logo></Logo>
       <img alt="The sulphur pile" className="SulphurImage" src="https://live.staticflickr.com/5253/5574931634_b8027c198d_b.jpg"></img>
       <div className="ButtonHolder">
-        <button>About Us</button>
-        <button>Testimonials</button>
-        <button>Contact Us</button>
+        <button onClick={navigateAbout}>About Us</button>
+        <button onClick={navigateTestimonials}>Testimonials</button>
+        
+        <button onClick={navigateContactUs}>Contact Us</button>
       </div>
     </div>
   );
 }
+ 
 
-export default App;
+
