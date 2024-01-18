bd60211fa487d973e03706ab9695df6024fe3ff6
Jet Simon <jetjdsimon@gmail.com>
2024-01-18 13:19:01 -0800

    Add basic homepage, placeholder buttons, logo

---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import { React } from 'react'
+import './Logo.css'
+
+function Logo() {
+    return (
+    <div className='Logo'>
+        <h1 className='LogoText'>The Big Yellow Sulphur Pile</h1>
+    </div>  
+    )
+}
+
+export default Logo;
