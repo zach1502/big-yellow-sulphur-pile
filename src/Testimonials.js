61ae212224bc2c133c8610008679d6b92aa59375
Jet Simon <jetjdsimon@gmail.com>
2024-01-19 17:20:53 -0800

    Add base for testimonials

--- src/Testimonials.js
+++ src/Testimonials.js
@@ -1,6 +1,36 @@
 import {useNavigate} from 'react-router-dom';
 import Logo from './components/Logo';
 import Testimonial from './components/Testimonial';
+import './Testimonials.css'
+
+const mockData = [
+  {
+    "review":"The finest dried urine anywhere!",
+    "reviewer":"Eric B",
+    "stars":5
+  },
+  {
+    "review":"It's big, it's yellow, it's a sulphur pile. Couldn't tick those boxes any better. View from the Grouse.",
+    "reviewer":"Hermione W",
+    "stars":5
+  },
+  {
+    "review":"I was disappointed to learn it wasn't a large pile of butter-flavoured popcorn seasoning",
+    "reviewer":"Phil M",
+    "stars":5
+  },
+  {
+    "review":"It truly is the biggest yellow sulphur pile you can find in the Vancouver harbour area.",
+    "reviewer":"Sylvain B",
+    "stars":5
+  },
+  {
+    "review":"Such a great big yellow Sulphur pile - just incredible! Not many words can describe the joy of finding out what the big yellow pile actually is!",
+    "reviewer":"Max B",
+    "stars":5
+  }
+]
+
 export default function Testimonials() {
     const navigate = useNavigate();
 
@@ -8,10 +38,17 @@ export default function Testimonials() {
       // navigate to /
       navigate('/');
     };
+
+    const makeTestimonials = (data) => {
+      return data.map((x) => <Testimonial key={x.reviewer+x.stars} review={x.review} reviewer={x.reviewer} stars={x.stars}/>);
+    }
+
     return (
-      <div className="Page">
-        <Logo text={"Testimonial"}></Logo>
-        <Testimonial review="The finest dried urine anywhere!" reviewer="Eric Buchanan" stars="5"/>
+      <div className='Page'>
+        <Logo text={'Testimonial'}></Logo>
+        <div className='Testimonials'>
+          {makeTestimonials(mockData)}
+        </div>
         <button onClick={navigateHome}>Return home</button>
       </div>
     );
